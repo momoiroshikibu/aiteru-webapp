8adbed571e4e4e5c8887f3d2f800fb04e34310b5
momoiroshikibu <momoiroshikibu@gmail.com>
2016-12-24 21:00:17 +0900

    add h2o.conf, add /assets/ directory

--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,6 @@
 module.exports = function hello() {
     console.log('hello');
+    fetch('/api/v1/places/').then(function(response) {
+        console.log(response.text());
+    })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const webpack = require('webpack');
 const COMMIT_HASH = process.env.COMMIT_HASH;
+
 module.exports = {
     context: __dirname + '/src',
     entry: {
         app: './app.js'
     },
     output: {
-        path: __dirname + '/dist',
+        path: __dirname + '/dist/assets',
         filename: `bundle.${COMMIT_HASH}.js`
     }
 };

c68c032ffc85a3fe6d5e57d2debf387b47f4cbbe
momoiroshikibu <momoiroshikibu@gmail.com>
2016-12-24 21:12:23 +0900

    fetch places with Authorization header

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,13 @@
 module.exports = function hello() {
     console.log('hello');
-    fetch('/api/v1/places/').then(function(response) {
-        console.log(response.text());
-    })
+    fetch('/api/v1/places/', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'b095a504-1dba-45c4-844b-9955c3435265'
+        }
+    }).then(function(response) {
+        return response.json();
+    }).then(function(places) {
+        console.log(places);
+    });
 }

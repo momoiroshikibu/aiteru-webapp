dfc64d0f66ebc4deedc295a22867fc5058a2b80e
momoiroshikibu <momoiroshikibu@gmail.com>
2016-12-31 13:05:00 +0900

    Drawer

--- src/scripts/AppBarComponent.jsx
+++ src/scripts/AppBarComponent.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import {Component} from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import Subheader from 'material-ui/Subheader';
+
 
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -9,23 +13,55 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 import TransitionUtil from './utils/TransitionUtil.es';
 
-export default function AppBarComponent(props) {
-    return (
-        <MuiThemeProvider muiTheme={getMuiTheme()}>
-            <AppBar
-                title="Aiteru?"
-                iconElementLeft={
-                    <IconMenu
-            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-            targetOrigin={{horizontal: 'left', vertical: 'top'}}>
-                        <MenuItem primaryText="places" onTouchTap={() => {TransitionUtil.emit('/places');}}/>
-                        <MenuItem primaryText="users" onTouchTap={() => {TransitionUtil.emit('/users');}}/>
-                    </IconMenu>
-                                }
-            />
-        </MuiThemeProvider>
-    )
+export default class AppBarComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            openDrawer: false
+        }
+    }
+
+    openDrawer() {
+        this.setState({
+            openDrawer: true
+        });
+    }
+
+    closeDrawer() {
+        this.setState({
+            openDrawer: false
+        });
+    }
+
+    navigate(path) {
+        TransitionUtil.emit(path);
+        this.closeDrawer();
+    }
+
+    render() {
+        return (
+            <div>
+                <MuiThemeProvider muiTheme={getMuiTheme()}>
+                    <AppBar
+                        title="Aiteru?"
+                        iconElementLeft="navigation-menu"
+                        iconElementLeft={<IconButton><MenuIcon onTouchTap={::this.openDrawer}/></IconButton>}
+                    />
+                </MuiThemeProvider>
+                <MuiThemeProvider muiTheme={getMuiTheme()}>
+                    <Drawer open={this.state.openDrawer}
+                            docked={false}
+                            onRequestChange={::this.closeDrawer}>
+                        <Subheader inset={true}>Menus</Subheader>
+                        <MenuItem primaryText="places" onTouchTap={() => {::this.navigate('/places')}}/>
+                        <MenuItem primaryText="users" onTouchTap={() => {::this.navigate('/users')}}/>
+                    </Drawer>
+                </MuiThemeProvider>
+            </div>
+        )
+    }
 }

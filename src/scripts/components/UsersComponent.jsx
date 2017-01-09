ad29dd5925ccfa2e01cc7871d2dd07078ae7d3e5
momoiroshikibu <momoiroshikibu@gmail.com>
2017-01-10 08:18:52 +0900

    refactored styles

--- src/scripts/components/UsersComponent.jsx
+++ src/scripts/components/UsersComponent.jsx
@@ -6,6 +6,21 @@ import Avatar from 'material-ui/Avatar';
 import ChanrioImage from './ChanrioImage.es';
 
 
+const styles = {
+    listItem: {
+        fontSize: '20pt',
+        minHeight: '20px'
+    },
+    avatar: {
+        marginTop: '6px'
+    },
+    userId: {
+        fontSize: '13px',
+        paddingTop: '6px',
+        textAlign: 'center'
+    }
+};
+
 export default class UsersComponent extends PresenterComponent {
 
     constructor({presenter}) {
@@ -21,24 +36,16 @@ export default class UsersComponent extends PresenterComponent {
         const presenter = super.getPresenter();
         const users = presenter.getUsers();
 
-
         const userListItems = users.map((user) => {
             const userId = user.id;
             return (
                 <ListItem key={userId}
                           primaryText={user.name}
-                          innerDivStyle={{
-                              fontSize: '20pt',
-                              minHeight: '20px'
-                          }}
+                          innerDivStyle={styles.listItem}
                           leftAvatar={<Avatar size={30}
-                                    style={{marginTop: '6px'}}
+                                    style={styles.avatar}
                                     src={`data:image/png;base64,${ChanrioImage}`} />}
-                          rightIcon={(<div style={{
-                              fontSize: '13px',
-                              paddingTop: '6px',
-                              textAlign: 'center'
-                          }}>{user.id}</div>)}
+                          rightIcon={(<div style={styles.userId}>{user.id}</div>)}
                           onClick={() => { presenter.navigate(userId); }}
                 />
             );
